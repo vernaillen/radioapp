5d571bfce8bb055b8183074f42fd4fc4e57db711
Wouter Vernaillen <wouter@vernaillen.com>
2023-07-01 16:09:28 +0200

    @vite-pwa/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  modules: ['@nuxthq/ui'],
+  modules: [
+    '@nuxthq/ui',
+    '@vite-pwa/nuxt'
+  ],
   devtools: { enabled: true }
 })
