ad1f5e3be17d27773f77f5c3f3218cc446de2852
Wouter Vernaillen <wouter@vernaillen.com>
2023-07-02 18:46:59 +0200

    DefaultOptions

--- composables/usePresets.ts
+++ composables/usePresets.ts
@@ -4,22 +4,7 @@ export const usePresets = () => {
   return [
     {
       name: 'Defaults',
-      options: {
-        mode: 5,
-        barSpace: 0.25,
-        gradient: 'rainbow',
-        ledBars: false,
-        lumiBars: false,
-        radial: false,
-        reflexAlpha: 0.25,
-        reflexBright: 1,
-        reflexFit: true,
-        reflexRatio: 0.3,
-        showBgColor: false,
-        showPeaks: true,
-        overlay: false,
-        height: 350
-      }
+      options: DefaultOptions,
     },
     {
       name: 'Classic LEDs',
