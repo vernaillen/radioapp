89363578811fc2ce20758615097baafea55ebae9
Wouter Vernaillen <wouter@vernaillen.com>
2023-07-03 14:37:14 +0200

    fullScreen mode & channels moved to composable

--- components/DemoAnalyzer.vue
+++ components/DemoAnalyzer.vue
@@ -6,80 +6,8 @@ import { useOptionsStore } from '@/stores/options'
 const optionsStore = useOptionsStore()
 const audio = ref<HTMLMediaElement>()
 const isPlaying = ref(false)
-onMounted(() => {
-    audio.value = document.getElementById('audio') as HTMLMediaElement
-    audio.value.onplaying = () => {
-        isPlaying.value = true
-    }
-    audio.value.onpause = () => {
-        isPlaying.value = false
-    }
-})
+const fullScreen = ref(false)
 
-const channels = [
-    {
-        label: 'ZenFM',
-        value: 'zenfm',
-        src: 'https://22653.live.streamtheworld.com/TOPZEN.mp3?lsid=app:54f1cdd7-7a00-42f8-9df5-64ebe6792dc9'
-    },
-    /*
-    {
-        label: 'StuBru UNTZ',
-        value: 'stubruuntz',
-        src: 'http://icecast.vrtcdn.be/stubru_untz-high.mp3'
-    },*/
-    {
-        label: 'BRUZZ',
-        value: 'bruzz',
-        src: 'https://i1.cdn.jetstre.am:8000/sz=fmbrussel=BRUZZ_HQ'
-    },
-    /*
-    {
-        label: 'URGent FM',
-        value: 'urgentfm',
-        src: 'http://urgentstream.radiostudio.be:8000/aac?fbclid=IwAR0UT-WBLUyWbUAmZbwbPExIyY7IDasgTpHbYFTdEUYBT08ARQ4KhxevaAk.mp3'
-    },*/
-    {
-        label: 'ORF Radio FM4',
-        value: 'orfmf4',
-        src: 'https://orf-live.ors-shoutcast.at/fm4-q2a'
-    },
-    {
-        label: 'WEFUNK Radio',
-        value: 'wefunk',
-        src: 'https://s-00.wefunkradio.com:8443/wefunk64.mp3'
-    },
-    {
-        label: 'We House Tunein Radio',
-        value: 'wehousetunein',
-        src: 'https://icecast9.play.cz/zun192.mp3'
-    },
-    {
-        label: 'SomaFM Beat Blender',
-        value: 'beatblender',
-        src: 'https://ice2.somafm.com/beatblender-128-mp3'
-    },
-    {
-        label: 'SomaFM Groove Salad',
-        value: 'groovesalad',
-        src: 'https://ice4.somafm.com/groovesalad-128-mp3'
-    },
-    {
-        label: 'SomaFM Suburbs of Goa',
-        value: 'suburbsofgoa',
-        src: 'https://ice2.somafm.com/suburbsofgoa-128-mp3'
-    },
-    {
-        label: 'SomaFM The Trip',
-        value: 'thetrip',
-        src: 'https://ice2.somafm.com/thetrip-128-mp3'
-    },
-    {
-        label: 'SomaFM Illinois Street Lounge',
-        value: 'illinoisstreetlounge',
-        src: 'https://ice6.somafm.com/illstreet-128-mp3'
-    }
-]
 function switchChannel (src: string, name: string) {
     const audioEl = document.getElementById('audio') as HTMLMediaElement
     audioEl.src = src
@@ -91,12 +19,21 @@ function switchChannel (src: string, name: string) {
 
 const channel = ref('wehousetunein')
 watch(channel, (newVal) => {
-    const channel = channels.find((channel) => channel.value === newVal)
+    const channel = useChannels().find((channel) => channel.value === newVal)
     if (channel) {
         switchChannel(channel.src, channel.label)
     }
 })
 
+onMounted(() => {
+    audio.value = document.getElementById('audio') as HTMLMediaElement
+    audio.value.onplaying = () => {
+        isPlaying.value = true
+    }
+    audio.value.onpause = () => {
+        isPlaying.value = false
+    }
+})
 optionsStore.updateOptions(usePresets()[4].options)
 </script>
 
@@ -108,11 +45,14 @@ optionsStore.updateOptions(usePresets()[4].options)
                 <UButton icon="i-heroicons-play" class="w-10 justify-center" v-if="!isPlaying" @click="audio?.play()"/>
                 <UButton icon="i-heroicons-pause" class="w-10 justify-center" v-if="isPlaying" @click="audio?.pause()"/>
             </div>
-            <div class="col-span-4 pr-3">
-                <USelect v-model="channel" :options="channels" color="primary" placeholder="Select channel..." />
+            <div class="col-span-3">
+                <USelect v-model="channel" :options="useChannels()" color="primary" placeholder="Select channel..." />
+            </div>
+            <div class="col-span-1 text-center">
+                <UButton icon="i-material-symbols-fullscreen" class="w-10 justify-center" @click="fullScreen = !fullScreen"/>
             </div>
         </div>
-        <VueAudioMotionAnalyzer :options="optionsStore.options" :source="audio" class="h-1/2" />
+        <VueAudioMotionAnalyzer :options="optionsStore.options" :source="audio" class="h-1/2" :full-screen="fullScreen" />
         <PresetSelector />
     </main>
 </template>
---
+++ composables/useChannels.ts
@@ -0,0 +1,66 @@
+export const useChannels = () => {
+    return [
+        {
+            label: 'ZenFM',
+            value: 'zenfm',
+            src: 'https://22653.live.streamtheworld.com/TOPZEN.mp3?lsid=app:54f1cdd7-7a00-42f8-9df5-64ebe6792dc9'
+        },
+        /*
+        {
+            label: 'StuBru UNTZ',
+            value: 'stubruuntz',
+            src: 'http://icecast.vrtcdn.be/stubru_untz-high.mp3'
+        },*/
+        {
+            label: 'BRUZZ',
+            value: 'bruzz',
+            src: 'https://i1.cdn.jetstre.am:8000/sz=fmbrussel=BRUZZ_HQ'
+        },
+        /*
+        {
+            label: 'URGent FM',
+            value: 'urgentfm',
+            src: 'http://urgentstream.radiostudio.be:8000/aac?fbclid=IwAR0UT-WBLUyWbUAmZbwbPExIyY7IDasgTpHbYFTdEUYBT08ARQ4KhxevaAk.mp3'
+        },*/
+        {
+            label: 'ORF Radio FM4',
+            value: 'orfmf4',
+            src: 'https://orf-live.ors-shoutcast.at/fm4-q2a'
+        },
+        {
+            label: 'WEFUNK Radio',
+            value: 'wefunk',
+            src: 'https://s-00.wefunkradio.com:8443/wefunk64.mp3'
+        },
+        {
+            label: 'We House Tunein Radio',
+            value: 'wehousetunein',
+            src: 'https://icecast9.play.cz/zun192.mp3'
+        },
+        {
+            label: 'SomaFM Beat Blender',
+            value: 'beatblender',
+            src: 'https://ice2.somafm.com/beatblender-128-mp3'
+        },
+        {
+            label: 'SomaFM Groove Salad',
+            value: 'groovesalad',
+            src: 'https://ice4.somafm.com/groovesalad-128-mp3'
+        },
+        {
+            label: 'SomaFM Suburbs of Goa',
+            value: 'suburbsofgoa',
+            src: 'https://ice2.somafm.com/suburbsofgoa-128-mp3'
+        },
+        {
+            label: 'SomaFM The Trip',
+            value: 'thetrip',
+            src: 'https://ice2.somafm.com/thetrip-128-mp3'
+        },
+        {
+            label: 'SomaFM Illinois Street Lounge',
+            value: 'illinoisstreetlounge',
+            src: 'https://ice6.somafm.com/illstreet-128-mp3'
+        }
+    ]
+}
